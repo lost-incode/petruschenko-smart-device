644f6a44c02a0a2c7a0c558e1b7479b036abe25c
lost <vicktoriaap@gmail.com>
2021-12-17 00:00:28 +0300

    Add mobile version of page

---
+++ source/js/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// var mobileAccordeonButtonSection = document.querySelector('.footer__mobile-button--sections');
+// var mobileAccordeonButtonOffice = document.querySelector('.footer__mobile-button--office');
+// var sectionsList = document.querySelector('.sections__list');
+// var officeWrapper = document.querySelector('.office__wrapper');
+
+// var mobileAccordeonButtons = document.querySelector('.footer__mobile-button');
+
+// for (var button in mobileAccordeonButtons) {
+//   button.classList.remove('visually-hidden');
+//   sectionsList.classList.add('disabled');
+//   officeWrapper.classList.add('disabled');
+//   console.log("fffff");
+// };
+
+// mobileAccordeonButtonSection.addEventListener('click', function (evt) {
+//   evt.preventDefault();
+//   mobileAccordeonButtonSection.classList.toggle('footer__mobile-button--hide');
+//   sectionsList.classList.toggle(('active'));
+//   if (officeWrapper.classList.contains('active')) {
+//     officeWrapper.classList.remove(('active'));
+//     officeWrapper.classList.add(('disabled'));
+//     mobileAccordeonButtonOffice.classList.toggle('footer__mobile-button--hide');
+//   }
+// });
+
+// mobileAccordeonButtonOffice.addEventListener('click', function (evt) {
+//   evt.preventDefault();
+//   mobileAccordeonButtonOffice.classList.toggle('footer__mobile-button--hide');
+//   officeWrapper.classList.toggle(('active'));
+//   if (sectionsList.classList.contains('active')) {
+//     sectionsList.classList.remove(('active'));
+//     sectionsList.classList.add(('disabled'));
+//     mobileAccordeonButtonSection.classList.toggle('footer__mobile-button--hide');
+//   }
+// });
